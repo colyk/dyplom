34b558a70334bd41aa7660af2ad8a4166ed67eb8
colyk <colyk@yandex.ru>
2019-08-11 20:55:36 +0200

    WIP

---
+++ geo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <CountryPicker/>
+  </div>
+</template>
+
+<script>
+import CountryPicker from './components/CountryPicker.vue';
+
+export default {
+  name: 'app',
+  components: {
+    CountryPicker,
+  },
+};
+</script>
+<style lang="stylus">
+#app
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
